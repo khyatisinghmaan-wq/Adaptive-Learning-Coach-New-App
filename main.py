be08333205226015b9fef81f864c2e04dc00de07
khyatisinghmaan-wq <khyatisinghmaan@gmail.com>
2025-11-07 19:12:34 -0800

    Modify app.run to read port from environment variable
    Update app.run to use PORT environment variable

--- main.py
+++ main.py
@@ -191,3 +191,5 @@ def metrics():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
